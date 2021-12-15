96cc754e10cde0506b2d33faa9129295499a504c
andrew <andrewnasief3.com>
2021-12-15 15:38:36 +0200

    added split

--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
+
 using namespace std;
 
-template<typename T ,int n>
+template<typename T, int n>
 struct Node {
-    T *value;
+    T **value;
     Node **child;
     int count;
     bool isLeaf;
@@ -12,114 +13,124 @@ public:
     Node() {
         count = 0;
         isLeaf = false;
-        this->value = new T[n];
+        this->value = new T *[n];
         this->child = new Node *[n];
-        for (int i = 0; i < n; i++) child[i] = nullptr;
+        for (int i = 0; i < n; i++) {
+            this->child[i] = nullptr;
+            this->value[i] = nullptr;
+        }
     }
 
     int addValue(T Value) {
         int i;
-        for (i = 0; i < n; i++)
-        {
+        for (i = 0; i < n; i++) {
             if (value[i] == nullptr) break;
-            if (Value > value[i]) break;
+            if (Value > *(value[i])) break;
         }
-        T temp = value[i];
-        for (int j = i; j < count; j++){
-            value[j+ 1] = value[j];
+        for (int j = i; j < count; j++) {
+            value[j + 1] = value[j];
         }
-        value[count] = temp;
+        value[i] = &Value;
         count++;
         return i;
     }
 
-    Node *findLeaf(Node<T,n> *root,int val) {
+    Node *findLeaf(Node<T, n> *root, int val) {
         bool flag = false;
         if (root->isLeaf) return root;
         else {
             for (int i = 0; i < count; i++) {
-                if(val < root->value[i]){
+                if (val < *(root->value[i])) {
                     flag = true;
-                    return findLeaf(root[i], val);
+                    return findLeaf(&(root[i]), val);
                 }
             }
-            if (!flag) return findLeaf(root[count], val);
+            if (!flag) return findLeaf(&(root[count]), val);
         }
     }
 
-    bool exist(T val){
-        for (int i = 0; i < count; i++){
-            if (val == value[i]) return true;
+    bool exist(T val) {
+        for (int i = 0; i < count; i++) {
+            if (val == *value[i]) return true;
         }
         return false;
     }
 };
 
-template<typename T,int n>
+template<typename T, int n>
 class BTree {
-    Node <T, n> *root;
+    Node<T, n> *root;
 public:
-    BTree(){
+    BTree() {
         root = new Node<T, n>;
         root->isLeaf = true;
     }
-    void insert(T value){
+
+    void insert(T value) {
         Node<T, n> *leaf = root->findLeaf(root, value);
         leaf->addValue(value);
-        //TODO SPLIT
+        if (leaf == root)
+            split(leaf, -1);
+        else
+            split(leaf, 0);
+
     }
-    void split(Node <T, n> *node, int flag){
-        if (node->count == n)
-        {
-            if (flag == -1)
-            {
+
+    void split(Node<T, n> *node, int flag) {
+        if (node->count == n) {
+            if (flag == -1) {
                 Node<T, n> *root = new Node<T, n>;
                 Node<T, n> *rightChild = new Node<T, n>;
-                T midValue = node->value[n/2];
-                node->value[n/2] = nullptr;
-                for (int i = n/2 + 1; i < n; i++)
-                {
-                    rightChild->addValue(node->value[i]);
+                T *midValue = node->value[n / 2];
+                node->value[n / 2] = nullptr;
+                for (int i = n / 2 + 1; i < n-1; i++) {
+                    rightChild->addValue(*(node->value[i]));
+                    rightChild->count++;
                     node->value[i] = nullptr;
+                    node->count--;
                 }
-                root->addValue(midValue);
+                rightChild->isLeaf= true;
+                root->addValue(*midValue);
+                node->count--;
+                node->isLeaf= true;
                 root->child[0] = node;
                 root->child[1] = rightChild;
                 this->root = root;
-            }
-            else{
-                T midValue = node->value[n/2];
-                node->valu[n/2] = nullptr;
-                Node<T, n> *parent = findParent(midValue);
-                int index = parent->addValue(midValue);
-                for (int i = index; i < parent->count - 1; i++){
+                root->isLeaf= true;
+
+            } else {
+                T *midValue = node->value[n / 2];
+                Node<T, n> *parent = findParent(*midValue);
+                node->value[n / 2] = nullptr;
+                int index = parent->addValue(*midValue);
+                for (int i = index + 1; i < parent->count - index + 1; i++) {
                     parent->child[i + 1] = parent->child[i];
                 }
-                parent->child[index] = new Node<T, n>;
-
+                parent->child[index + 1] = new Node<T, n>;
+                for (int i = n / 2 + 1; i < n; i++) {
+                    parent->child[index + 1]->addValue(*node->value[i]);
+                    node->value[i] = nullptr;
+                }
             }
         }
     }
-    Node <T, n> *findParent(T value)
-    {
-        Node <T, n> *current = root;
-        Node <T, n> *parent;
-        while (!current->exist(value))
-        {
+
+    Node<T, n> *findParent(T value) {
+        Node<T, n> *current = root;
+        Node<T, n> *parent;
+        while (!current->exist(value)) {
             bool found = false;
-            for (int i = 0; i < current->count; i++)
-            {
-                if (value < current->value[i])
-                {
+            for (int i = 0; i < current->count; i++) {
+                if (value < *current->value[i]) {
                     found = true;
                     parent = current;
                     current = current->child[i];
                     break;
                 }
             }
-            if (!found){
+            if (!found) {
                 parent = current;
-                current = current->child[count];
+                current = current->child[current->count];
             }
         }
         return parent;
@@ -127,7 +138,13 @@ public:
 };
 
 int main() {
-    BTree<int,5> s;
+    BTree<char, 4> s;
+    s.insert('a');
+    s.insert('b');
+    s.insert('c');
+    s.insert('d');
+    s.insert('e');
+    s.insert('x');
 
     return 0;
 }
